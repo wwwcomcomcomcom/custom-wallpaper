54d1d00e263b62fb6cd493e23a25a8ec4ed566b9
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-11 21:02:33 +0900

    just format code

--- src/App.svelte
+++ src/App.svelte
@@ -1,104 +1,104 @@
 <script type="module" lang="ts">
-import Clock from "./lib/Clock.svelte";
-import {type GLTF, GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
-import * as Three from "three";
-import {onMount} from "svelte";
+    import Clock from "./lib/Clock.svelte";
+    import {type GLTF, GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+    import * as Three from "three";
+    import {onMount} from "svelte";
 
-function addChicken(positionZ:number,scene:Three.Scene,renderer:Three.WebGLRenderer, camera:Three.PerspectiveCamera) {
-    const loader = new GLTFLoader();
-    loader.load("/chicken-model/scene.gltf",(model) => {
-        scene.add(model.scene);
-        model.scene.position.z = positionZ;
-        model.scene.rotation.y = positionZ/260*2*Math.PI;
-        let tick = (positionZ+200)/260*60;
-        function animate() {
-            requestAnimationFrame(animate);
-
-            // model.scene.rotation.x += Math.PI/70;
-            model.scene.rotation.y += Math.PI/50;
-            model.scene.position.z += 0.7;
-            // model.scene.position.x += Math.sin(Math.PI/30*tick++)/2;
-            if(tick >= 60) tick = 0;
-            if(model.scene.position.z > 60) {
-                model.scene.position.z = -200;
-            }
-            renderer.render(scene,camera);
-        }
-        animate();
-    });
-}
-
-function loadModel(modelPath:sting) {
-    return new Promise<GLTF>((resolve,reject) => {
+    function addChicken(positionZ:number,scene:Three.Scene,renderer:Three.WebGLRenderer, camera:Three.PerspectiveCamera) {
         const loader = new GLTFLoader();
-        loader.load(modelPath,model => resolve(model),null,reject);
-    });
-}
-
-let canvas:HTMLCanvasElement;
-onMount(async ()=>{
-    let scene = new Three.Scene();
-    let renderer = new Three.WebGLRenderer({
-        canvas: canvas!,
-        antialias: true,
-    });
-
-    renderer.outputColorSpace = Three.SRGBColorSpace;
-    let camera = new Three.PerspectiveCamera(30,1);
-    camera.position.set(0,50,100);
-    camera.lookAt(0,0,0);
-
-    // const material = new Three.MeshBasicMaterial();
-    // material.color = new Three.Color(0xff55aa);
-    // const geometry = new Three.BoxGeometry(200,1,200);
-    // const cube = new Three.Mesh(geometry,material);
-    // cube.position.set(0,-10,0);
-    // scene.add(cube);
-
-    scene.background = new Three.Color(0xff55aa);
-    let light = new Three.DirectionalLight(0xffffff,3);
-    light.position.set(0,50,100);
-    scene.add(light);
-
-    const positionGap = 260/10;
-    let chickens:GLTF[][] = [];
-
-    for(let j = 0; j < 5; j++) {
-        const temp = [];
-        for(let i = -200; i < 60; i+=positionGap) {
-            temp.push(await loadModel("/chicken-model/scene.gltf"));
-        }
-        chickens.push(temp);
-    }
-    let line = -30;
-    chickens.forEach((chickens) => {
-        chickens.forEach((chicken,i) => {
-            const positionZ = i*positionGap - 200;
-            chicken.scene.position.x = line;
-            chicken.scene.position.z = positionZ;
-            chicken.scene.rotation.y = positionZ/260*2*Math.PI;
-            scene.add(chicken.scene);
+        loader.load("/chicken-model/scene.gltf",(model) => {
+            scene.add(model.scene);
+            model.scene.position.z = positionZ;
+            model.scene.rotation.y = positionZ/260*2*Math.PI;
+            let tick = (positionZ+200)/260*60;
             function animate() {
                 requestAnimationFrame(animate);
-                chicken.scene.rotation.y += Math.PI/50;
-                chicken.scene.position.z += 0.7;
-                if(chicken.scene.position.z > 60) {
-                    chicken.scene.position.z = -200;
+
+                // model.scene.rotation.x += Math.PI/70;
+                model.scene.rotation.y += Math.PI/50;
+                model.scene.position.z += 0.7;
+                // model.scene.position.x += Math.sin(Math.PI/30*tick++)/2;
+                if(tick >= 60) tick = 0;
+                if(model.scene.position.z > 60) {
+                    model.scene.position.z = -200;
                 }
                 renderer.render(scene,camera);
             }
             animate();
         });
-        line += 15;
-    })
-});
+    }
+
+    function loadModel(modelPath:sting) {
+        return new Promise<GLTF>((resolve,reject) => {
+            const loader = new GLTFLoader();
+            loader.load(modelPath,model => resolve(model),null,reject);
+        });
+    }
+
+    let canvas:HTMLCanvasElement;
+    onMount(async ()=>{
+        let scene = new Three.Scene();
+        let renderer = new Three.WebGLRenderer({
+            canvas: canvas!,
+            antialias: true,
+        });
+
+        renderer.outputColorSpace = Three.SRGBColorSpace;
+        let camera = new Three.PerspectiveCamera(30,1);
+        camera.position.set(0,50,100);
+        camera.lookAt(0,0,0);
+
+        // const material = new Three.MeshBasicMaterial();
+        // material.color = new Three.Color(0xff55aa);
+        // const geometry = new Three.BoxGeometry(200,1,200);
+        // const cube = new Three.Mesh(geometry,material);
+        // cube.position.set(0,-10,0);
+        // scene.add(cube);
+
+        scene.background = new Three.Color(0xff55aa);
+        let light = new Three.DirectionalLight(0xffffff,3);
+        light.position.set(0,50,100);
+        scene.add(light);
+
+        const positionGap = 260/10;
+        let chickens:GLTF[][] = [];
+
+        for(let j = 0; j < 5; j++) {
+            const temp = [];
+            for(let i = -200; i < 60; i+=positionGap) {
+                temp.push(await loadModel("/chicken-model/scene.gltf"));
+            }
+            chickens.push(temp);
+        }
+        let line = -30;
+        chickens.forEach((chickens) => {
+            chickens.forEach((chicken,i) => {
+                const positionZ = i*positionGap - 200;
+                chicken.scene.position.x = line;
+                chicken.scene.position.z = positionZ;
+                chicken.scene.rotation.y = positionZ/260*2*Math.PI;
+                scene.add(chicken.scene);
+                function animate() {
+                    requestAnimationFrame(animate);
+                    chicken.scene.rotation.y += Math.PI/50;
+                    chicken.scene.position.z += 0.7;
+                    if(chicken.scene.position.z > 60) {
+                        chicken.scene.position.z = -200;
+                    }
+                    renderer.render(scene,camera);
+                }
+                animate();
+            });
+            line += 15;
+        })
+    });
 
 </script>
 
 <main>
     <Clock />
     <canvas bind:this={canvas} id="canvas" width="1920" height="1080"></canvas>
-<!--    <BackgroundGrid/>-->
+    <!--<BackgroundGrid/>-->
 </main>
 
 <style>
